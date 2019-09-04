c11a47aec1ee86121d5ddd2447c037cf0d3e6fce
Kieran Barker <kieran11397@gmail.com>
2019-09-04 12:04:41 +0100

    Add finished script

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
 (function(d, u) {
 
+  //
+  // Variables
+  //
+
+  var content = d.getElementById("content");
+  var wordCount = d.getElementById("word-count");
+
+  //
+  // Functions
+  //
+
+  var countWords = function(element) {
+    if (element.nodeName.toLowerCase() !== "textarea" && element.nodeName.toLowerCase() !== "input" && element.type.toLowerCase() !== "text") return;
+
+    var wordCount = element.value.trim();
+    if (!wordCount) return 0;
+
+    wordCount = wordCount.split(/\s+/).length;
+    return wordCount;
+  };
+
+  var updateWordCount = function(content, wordCount) {
+    if (!content || !wordCount) return;
+    wordCount.textContent = countWords(content);
+  };
+
+  //
+  // Init
+  //
+
+  content.addEventListener("input", function() {
+    updateWordCount(this, wordCount);
+  }, false);
+
 })(document);

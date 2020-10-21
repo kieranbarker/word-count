01a822d9364135e1a43ff17c702a9ecb238d0cee
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2020-10-21 10:48:26 +0100

    Update script to ES6

--- app.js
+++ app.js
@@ -1,68 +1,57 @@
-;(function () {
+//
+// Variables
+//
 
-  // Opt into ES5 strict mode
-  "use strict";
+// Get the #text element
+const textArea = document.querySelector('#text');
 
-  //
-  // Variables
-  //
-
-  // Get the #text element
-  var text = document.querySelector("#text");
-
-  // Get the #count element
-  var count = document.querySelector("#count");
+// Get the #count element
+const count = document.querySelector('#count');
 
 
-  //
-  // Functions
-  //
+//
+// Functions
+//
 
-  /**
-   * Get the number of words in a text area
-   * @param   {Object} textArea The text area
-   * @returns {Number}          The word count
-   */
-  function getWordCount (textArea) {
+/**
+ * Get the number of words inside a form field
+ * @param {HTMLInputElement|HTMLTextAreaElement} field The form field
+ * @returns {Number} The word count
+ */
+function getWordCount (field) {
+  // Trim whitespace from the value
+  const value = field.value.trim();
+  
+  // If it's an empty string, return zero
+  if (!value) return 0;
 
-    // Trim whitespace from the value
-    var value = textArea.value.trim();
-    
-    // If it's an empty string, return zero
-    if (!value) return 0;
+  // Otherwise, return the word count
+  return value.split(/\s+/).length;
+}
 
-    // Otherwise, return the word count
-    return value.split(/\s+/).length;
+/**
+ * Get the number of characters inside a form field
+ * @param {HTMLInputElement|HTMLTextAreaElement} field The form field
+ * @returns {Number} The character count
+ */
+function getCharacterCount (field) {
+  return field.value.length;
+}
 
-  }
-
-  /**
-   * Get the number of characters in a text area
-   * @param   {Object} textArea The text area
-   * @returns {Number}          The character count
-   */
-  function getCharacterCount (textArea) {
-    return textArea.value.length;
-  }
-
-  /**
-   * Update the word and character counts
-   */
-  function updateCounts () {
-
-    count.textContent = (
-      "You've written " + getWordCount(this) + " words " +
-      "and " + getCharacterCount(this) + " characters."
-    );
-
-  }
+/**
+ * Handle input events
+ */
+function handleInput () {
+  count.textContent = `
+    You've written ${getWordCount(this)} words
+    and ${getCharacterCount(this)} characters.
+  `;
+}
 
 
-  //
-  // Inits & Event Listeners
-  //
+//
+// Inits & Event Listeners
+//
 
-  // Update both counts when the value of #text changes
-  text.addEventListener("input", updateCounts);
-
-})();
+// Handle input events
+textArea.addEventListener('input', handleInput);
